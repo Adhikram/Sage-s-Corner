b204c8cf7c2ab9b73b55777de8c743aace6fa351
Adhikram <adhikramm@gmail.com>
2021-07-23 19:31:19 +0530

    🌱

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name: 'Adhikram Maitra',
              activated: true,
              activated_at: Time.zone.now)
 # Generate a bunch of additional users.
-99.times do |n|
+9.times do |n|
   name = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   password = 'password'
@@ -20,7 +20,7 @@ User.create!(name: 'Adhikram Maitra',
 end
 # Generate microposts for a subset of users.
 users = User.order(:created_at).take(6)
-50.times do
+5.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each do |user|
     user.microposts.create!(content:
